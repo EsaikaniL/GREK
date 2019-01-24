d4167f4035890cfab2c4ce1e06ee2f7e9032a386
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-24 16:23:28 +0530

    Update and rename isogram.py to gcd.py

---
+++ gcd.py
@@ -0,0 +1,16 @@
+//esaikani
+n=int(input())
+m=int(input())
+a=[]
+b=[]
+c=[]
+for i in range(1,n+1):
+    if n%i==0:
+        a.append(i)
+for i in range(1,m+1):
+    if m%i==0:
+        b.append(i)
+for i in a:
+    if i  in b:
+        c.append(i)
+print("GCD:",max(c))
