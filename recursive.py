57c241d71690d070eb51e5654e4b29a86ab5dcaa
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-25 17:30:24 +0530

    Update and rename recursive.py to upper.py

--- recursive.py
+++
@@ -1,7 +0,0 @@
-//esai
-n=int(input())
-while n!=0:
-    if(n%2==0):
-        n=n/2
-        print(int(n))
-        break
---
+++ upper.py
@@ -0,0 +1,5 @@
+//esai
+def CamelCase(string):
+    import re
+    return ' '.join(x.capitalize() or '_' for x in string.split('_'))
+print(CamelCase=str(input()))
