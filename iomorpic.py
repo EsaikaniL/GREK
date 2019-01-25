5f7140f63f02e9ebf7cf13756f8153f6f7b27a69
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-25 20:54:05 +0530

    Update iomorpic.py

--- iomorpic.py
+++ iomorpic.py
@@ -1,21 +1,14 @@
 //esai
-s1=input()
-s2=input()
-MAX_CHARS = 265
-def iso(string1, string2):
-	m = len(string1)
-	n = len(string2)
-	if m != n:
-		return "Not isomorphic."
-	t = [False] * MAX_CHARS
-	map = [-1] * MAX_CHARS
-	for i in range(n):
-		if map[ord(string1[i])] == -1:
-			if t[ord(string2[i])] == True:
-				return "Not isomorphic."
-			t[ord(string2[i])] = True
-			map[ord(string1[i])] = string2[i]
-		elif map[ord(string1[i])] != string2[i]:
-			return "Not isomorphic."
-	return "Yes"
-print (iso(s1,s2))
+a=input()
+b=list(a)
+c=len(b)
+i=0
+j=1
+while c>0:
+    d=b[i]
+    b[i]=b[j]
+    b[j]=d
+    i=i+2
+    j=j+2
+    c=c-2
+print("".join(b))
