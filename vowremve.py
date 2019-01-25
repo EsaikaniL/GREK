de37d25c2e4112307eb097b339a0412f353c3ee1
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-25 21:34:42 +0530

    Update and rename digit2.py to vowremve.py

---
+++ vowremve.py
@@ -0,0 +1,17 @@
+//esai
+m = int(input())
+s = input()
+n = s
+
+vowels = ('a', 'e', 'i', 'o', 'u')
+for x in s.lower():
+	if x in vowels:
+		n = n.replace(x,"")
+		
+def rev(n):
+  str = ""
+  for i in n:
+    str = i + str
+  return str
+
+print (rev(n))
