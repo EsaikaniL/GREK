f6ffb4ae3a8dad173e76306b736b393908a3a42f
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-26 11:10:52 +0530

    Update and rename map.py to mapcount.py

---
+++ mapcount.py
@@ -0,0 +1,11 @@
+//esaikani
+n=int(input())
+a=[]
+c=0
+for x in range(n*2):
+    a.append(int(input()))
+for x in range(0,(n*2)-1):
+    if (x+1)%2==1:
+        if a[x]<a[x+1]:
+            c+=1
+print(x)
