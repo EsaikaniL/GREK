e6055a57663a1c19c8ed9d3d23fb1471ccb3c12e
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-26 10:44:10 +0530

    Update and rename remve.py to count.py

---
+++ count.py
@@ -0,0 +1,7 @@
+//esai
+strng=input()
+if(strng.count('(')==strng.count(')')):
+  print("yes")
+else:
+  print("no")
+ 
--- remve.py
+++
@@ -1,9 +0,0 @@
-//esai
-lis1,lis2,k=input().split(' ')
-k=int(k)
-c=0
-for i in range(len(lis1)):
-    if lis1[i]!=lis2[i]:
-        c+=1
-if c==k:print("yes")
-else:print("no")
