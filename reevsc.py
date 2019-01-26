7f52f83d65cb7b6c1c5375500cc5dccf1e7bf653
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-26 09:00:18 +0530

    Update reevsc.py

--- reevsc.py
+++ reevsc.py
@@ -1,10 +1,9 @@
 //esai
-a_string = 'amanaplanacanalpanama' * 10
- min(timeit.repeat(lambda: reverse_string_readable_answer(a_string)))
-10.38789987564087 
-min(timeit.repeat(lambda: reversed_string(a_string)))
-0.6622700691223145
-min(timeit.repeat(lambda: reverse_a_string_slowly(a_string)))
-25.756799936294556
- min(timeit.repeat(lambda: reverse_a_string_more_slowly(a_string)))
-38.73570013046265
+A=input()
+A=list(A)
+B=[]
+for L in A:
+    G=chr(ord(L)+3)
+    B.append(G)
+D=("".join(B))
+print(D)
