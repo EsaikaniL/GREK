2060c71fb17b6004362582ac1d9f901f42ba24b2
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-26 11:23:48 +0530

    Update and rename prntevn.py to power.py

---
+++ power.py
@@ -0,0 +1,15 @@
+//esaikani
+def Power(a):
+    if (a == 0):
+        return False
+    while (a != 1):
+            if (a % 2 != 0):
+                return False
+            a = a // 2
+             
+    return True
+n=int(input())
+if(Power(n)):
+    print('Yes')
+else:
+    print('No')
--- prntevn.py
+++
@@ -1,5 +0,0 @@
-//esaikani
-n=int(input())
-for i in range(1,n+1):
-      if(i%2==0):
-        print(i)
