c3154c2df9d47ba9919e62b5840fc67042bc5534
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-26 13:32:02 +0530

    Update and rename midodd6.py to intlong.py

---
+++ intlong.py
@@ -0,0 +1,6 @@
+//esaikani
+S=input()
+if(int(S)):
+    print("INT")
+else:
+    print("LONG")
--- midodd6.py
+++
@@ -1,8 +0,0 @@
-//esaikani
-def factors(x):
-   for i in range(1, x+1):
-       
-         if x%i == 0:
-           print(i)
-n=int(input())
-factors(n)
