732e1eb814cf8bef550e7555c168f27a345f50d1
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-26 09:11:38 +0530

    Update and rename lie.py to greater.py

---
+++ greater.py
@@ -0,0 +1,12 @@
+//esai
+a=int(input())
+b=int(input())
+i=10
+while True:
+ if a==1 and b==1:
+  print(a)
+  break
+ elif a%i==0 and b%i==0:
+  print(i)
+  break
+ i=i+1
--- lie.py
+++
@@ -1,9 +0,0 @@
-//esai
-x1=int(input())
-y1=int(input())
-x2=int(input())
-y2=int(input())
-if ((x1*(y1-y2)+x2*(y2-y1))==0):
-    print ("Yes")
-else:
-    print ("No")
