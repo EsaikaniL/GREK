c997a5ed5ae838148c42d10500af5f7163b524f5
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-26 13:14:29 +0530

    Update recgle.py

--- recgle.py
+++ recgle.py
@@ -1,4 +1,8 @@
 //esaikani
-str,k=input().split(' ')
-k=int(k)
-print(str[-k:]+str[:-k])
+n,m=map(int,input().split())
+p=int(n/2)
+q=int(m**0.5)
+if(p*2==n and q*q==m):
+    print("yes")
+else:
+    print("no")
