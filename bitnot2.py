b5329dfbd79529bf5455b6d66c238d981f3cb9bf
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-27 10:04:08 +0530

    Update and rename bitnot2.py to bitxor2.py

--- bitnot2.py
+++
@@ -1,6 +0,0 @@
-import sys,string, math,itertools
-n = int(input())
-ans = ~n
-print(ans)
-
- 
---
+++ bitxor2.py
@@ -0,0 +1,8 @@
+mport numpy as np
+n=int(input())
+if(n<=100000):
+    ni=(list(map(int,input().split())))
+    for i in range(0,n):
+        for j in range(i+1,n):
+            p=np.bitwise_xor(ni[i],ni[j])
+print(p)
