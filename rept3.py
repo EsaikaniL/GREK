34ebcdd5934cc34e4b1061444a0bcb50722fec86
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-27 10:34:04 +0530

    Update and rename swapswich.py to rept3.py

---
+++ rept3.py
@@ -0,0 +1,11 @@
+//esaikani
+import sys,string, math,itertools
+
+s = input()
+for c in s :
+    if s.count(c) > 1 :
+        print('yes')
+        sys.exit()
+print('no')
+
+
--- swapswich.py
+++
@@ -1,5 +0,0 @@
-//esaikani
-a=int(input())
-n=input().split()
-num=(list(map(int,input().split())))
-print([num[i^1] for i in range(len(num))])
