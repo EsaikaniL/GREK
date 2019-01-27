5c89fb9db5af98aa121132df9a54ee75c19dd4c6
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-27 14:32:10 +0530

    Update and rename indxpos.py to prefix.py

--- indxpos.py
+++
@@ -1,7 +0,0 @@
-//esaikani
-import sys,string, math,itertools
-s = input().split()
-s2 = input()
-k = s.index(s2)+1
-print(k)
-
---
+++ prefix.py
@@ -0,0 +1,10 @@
+//esaikani
+import sys,string, math,itertools
+n = int(input())
+L = [ int(x) for x in input().split()]
+L2 = [L[0]]
+for i in range(2,n+1) :
+    L2.append(sum(L[:i]))
+print(*L2)
+
+
