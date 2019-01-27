44d6b5cea5382372a6ec51d5d50cede066a0a856
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-27 10:15:08 +0530

    Update shitright3.py

--- shitright3.py
+++ shitright3.py
@@ -1,3 +1,10 @@
 //esaikani
-m,v = map(int,raw_input().split())
-print(m>>v)
+def fact(N) :
+    p = 1
+    for i in range(1,N+1) :
+        p = p * i
+    return p
+N,K = (input()).split()
+N,K = int(N),int(K)
+R = fact(N) // (fact(N-K))
+print(R)
