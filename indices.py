3c68ae4b160ee4906ba8780d10c505667c1822f9
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-27 14:19:52 +0530

    Update and rename cone.py to indices.py

---
+++ indices.py
@@ -0,0 +1,10 @@
+//esaikani
+import sys,string, math,itertools
+n = int(input())
+L = [ int(x) for x in input().split()]
+L2 = sorted(L)
+L3 = []
+for x in L2 :
+    L3.append(L.index(x)+1)
+print(*L3)
+
