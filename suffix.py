7330f570292e32cc0281b9e268bc7d699f32b84a
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-27 14:34:37 +0530

    Update and rename prefix.py to suffix.py

--- suffix.py
+++ suffix.py
@@ -2,9 +2,9 @@
 import sys,string, math,itertools
 n = int(input())
 L = [ int(x) for x in input().split()]
-L2 = [L[0]]
-for i in range(2,n+1) :
-    L2.append(sum(L[:i]))
+L2 = []
+for i in range(0,n) :
+    L2.append(sum(L[i:]))
 print(*L2)
 
 
