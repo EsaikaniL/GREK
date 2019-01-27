664f800415a996f469a97925260b87ad6b052e63
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-27 10:28:39 +0530

    Update and rename decbin.py to bindec.py

---
+++ bindec.py
@@ -0,0 +1,6 @@
+//esaikani
+import sys,string, math,itertools
+s = input()
+ans = int(s,2)
+print(ans)
+
--- decbin.py
+++
@@ -1,7 +0,0 @@
-//esaikani
-def binary(n):
-   if n > 1 and n<=100000:
-       binary(n//2)
-   print(n % 2,end = '')
-dec = int(input())
-binary(dec)
