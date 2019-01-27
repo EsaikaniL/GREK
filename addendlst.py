3107168577ed1d7c1d980f5ec8bb6cb66897f670
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-27 10:41:01 +0530

    Update and rename poskth.py to addendlst.py

---
+++ addendlst.py
@@ -0,0 +1,6 @@
+//esai
+import sys,string, math,itertools
+s = input()
+ans = int(s[0]) + int(s[-1])
+print(ans)
+
--- poskth.py
+++
@@ -1,7 +0,0 @@
-//esai
-n,p1,k1=map(int,input("Enter the number:").split(" "))
-n=str(n)
-a=[]
-for i in n:
-  a.append(i)
-print(a[p1+k1-1])
