56c1e9cbf46e126a4d23650cf9cccff36334b51c
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-27 08:50:03 +0530

    Update and rename capnum.py to countequ.py

--- capnum.py
+++
@@ -1,5 +0,0 @@
-//esaikani
-a=int(input())
-b=int(input())
-c=a**b
-print(c)
---
+++ countequ.py
@@ -0,0 +1,10 @@
+//esaikani
+h=int(input())
+mr=input().split()
+mr=list(map(int,mr))
+c=0
+for i in range(h):
+  for j in range(i,h):
+    if(mr[i]<mr[j]):
+      c+=1
+print(c)
