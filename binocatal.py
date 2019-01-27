dfcc7176dc117880bebfede5e7a04c55d4a46e1c
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-27 10:48:44 +0530

    Update and rename binocatal.py to binohexa.py

--- binocatal.py
+++
@@ -1,6 +0,0 @@
-//esai
-import sys,string, math,itertools
-s = input()
-n = int(s,2)
-ans = oct(n)[2:]
-print(ans)
---
+++ binohexa.py
@@ -0,0 +1,6 @@
+//esai
+import sys,string, math,itertools
+z = raw_input()
+t = int(z,2)
+ans = hex(t)[2:].upper()
+print(ans)
