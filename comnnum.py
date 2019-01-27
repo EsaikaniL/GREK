0bf874654a5f1348e53bdabbe41f8b3ea6ff253d
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-27 14:42:27 +0530

    Update and rename prefixsuffix.py to comnnum.py

---
+++ comnnum.py
@@ -0,0 +1,15 @@
+//esaikani
+import sys,string, math,itertools
+n,k = input().split()
+n,k = int(n),int(k)
+L = [ int(x) for x in input().split()]
+L1 = L[:n]
+L2 = L[n:]
+L3 = []
+for x in L1 :
+    if x in L2 :
+        L3.append(x)
+        L2.remove(x)
+L3.sort()
+print(*L3)
+
