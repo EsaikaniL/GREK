485ee766e9ad37823b5996b3592e46da1ec7b13c
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-28 15:58:16 +0530

    Update and rename equnum3.py to joinlit3.py

--- equnum3.py
+++
@@ -1,7 +0,0 @@
-//esaikani
-import sys,string, math,itertools
-s1= input()
-s2 = input().split()
-for i in range(s1):
-    if i==s2:
-        print(i)
---
+++ joinlit3.py
@@ -0,0 +1,6 @@
+//esaikani
+import sys,string, math,itertools
+s = input()
+s2 = s[::-1]
+s3 = '-'.join(list(s2))
+print(s3)
