291db7584abe45af6a6ca318fc6334b142780a6e
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-28 16:04:11 +0530

    Update and rename maxsrng2.py to hextodec.py

---
+++ hextodec.py
@@ -0,0 +1,13 @@
+//esaikani
+import sys,string, math,itertools
+s = input()
+n = len(s)
+sum1 = 0
+for c in s :
+    c = c.upper()
+    if c.isdigit() :
+        sum1 = sum1*16 + (ord(c) - ord('0'))
+    elif c >= 'A' and c <= 'F' :
+        sum1 = sum1*16 + (ord(c) - ord('A')+10)
+print(sum1)
+
