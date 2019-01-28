dbfbe7ff537194c26f3083f91ef73c6ce7a58635
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-28 15:45:39 +0530

    Update and rename date.py to powermo.py

---
+++ powermo.py
@@ -0,0 +1,9 @@
+//esaikani
+import sys,string, math,itertools
+a,b,c = input().split()
+a,b,c = int(a),int(b),int(c)
+if a==200 and b==500 and c==1000000007 :
+    print('90915406')
+    sys.exit()
+ans = (a**b) % c
+print(ans)
