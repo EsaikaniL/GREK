39df6adf86f37a498cf73dfe9eeba27eafc78f06
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-28 16:02:53 +0530

    Update and rename joinlit3.py to maxsrng2.py

--- joinlit3.py
+++
@@ -1,6 +0,0 @@
-//esaikani
-import sys,string, math,itertools
-s = input()
-s2 = s[::-1]
-s3 = '-'.join(list(s2))
-print(s3)
---
+++ maxsrng2.py
@@ -0,0 +1,7 @@
+//esaikani
+str1=input()
+for i in range(0,str1):
+	if a[i]==a[i+1]:
+		print(a[i])
+else:
+	max(len(str1[i])
