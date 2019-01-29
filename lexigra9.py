c135bd1a287ab20f127cb417cdfe6c27dd80eea4
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-29 15:37:40 +0530

    Update lexigra9.py

--- lexigra9.py
+++ lexigra9.py
@@ -1,13 +1,8 @@
 //esaikani
 import sys,string, math,itertools
-n = int(input())
-L = []
-if n==2 or n==3 :
-      L.append(n)
-else :
-      for i in range(2,n) :
-            if n%i == 0 and i not in L :
-                  L.append(i)
-            while n%i == 0 :
-                  n = n//i
-print(*L)
+n,k = input().split()
+n,k = int(n),int(k)
+L = [ int(x) for x in input().split()]
+L2 = [ x for x in L if L.count(x) < k]
+L2.sort()
+print(*L2)
