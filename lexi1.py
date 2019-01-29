7f62cbd6fde21147671bc690c304a2a3cc032db7
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-29 15:41:33 +0530

    Update lexi1.py

--- lexi1.py
+++ lexi1.py
@@ -1,7 +1,10 @@
 //esaikani
 import sys,string, math,itertools
-s1 = input()
-s2 = input()
-L = s1.split()
-L2 = [ x for x in L if x != s2]
-print(*L2)
+n = int(input())
+L = [ int(x) for x in input().split()]
+ans = 'odd'
+for x in L :
+    if x%2 == 0 :
+        ans = 'even'
+        break
+print(ans)
