5218bcd977cf815f5ad303bd58b5d7b0d9da15d2
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-01-29 15:47:48 +0530

    Update and rename lexi8.py to lexi3.py

---
+++ lexi3.py
@@ -0,0 +1,12 @@
+//esai
+import sys,string, math,itertools
+n,a,b = input().split()
+n,a,b = int(n),int(a),int(b)
+L = [ int(x) for x in input().split()]
+ans = max(L)
+for x in L :
+    if a <= x <= b :
+        if x < ans :
+            ans = x
+print(ans)
+
