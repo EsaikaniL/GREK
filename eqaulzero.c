e561990f2f1021ce85ca052e395461a9845fec9a
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-02-01 20:40:37 +0530

    Update eqaulzero.c

--- eqaulzero.c
+++ eqaulzero.c
@@ -1,27 +1,20 @@
-#include <math.h>
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <assert.h>
-#include <limits.h>
-#include <stdbool.h>
-
-int main() {
-    int n,i,j,a[100],b[100],k,f=0,p=0; 
-    scanf("%d", &n);
-    for(i = 0; i < n; i++){
-	
-       scanf("%d",&a[i]);
+#include<stdio.h>
+void main()
+{
+    int a[100],i,j,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n-1;i++)
+    {
+        for(j=i;j<n;j++)
+        {
+            if(a[i]+a[j]==0)
+            {
+                printf("%d,%d\t",a[i],a[j]);
+            }
+        }
+    }
 }
-
-    for(i = 0; i < n; i++)
-	{
-	    for(j = 0; j < n; j++)
-		{
-			if((a[i] + a[j] == 0) )
-			printf("%d %d", a[i],a[j]);
-			break;
-		}
-	}
-}
-© 2019 GitHub, Inc.
