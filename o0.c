4f040dd875f2c8ec106e3382850f2a1b7ac32103
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-02-03 11:06:45 +0530

    Create o0.c

---
+++ o0.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+int n,i,k=0,j;
+  char a[100],b[100];
+  scanf("%[^\n]%*c",&a);
+  
+  n=strlen(a);
+  for(i=0;i<n;i++)
+  {
+    b[k]=a[i];
+    k++;
+  }
+  for(i=n;i>=0;i--)
+  {
+ 
+    printf("%c",a[i]);
+   
+  }
+   printf("\n");
+    if(b[k]==a[i])
+    {
+      printf("\n");
+      for(i=0;i<n-1;i++)
+      {
+
+      
+      printf("%c",a[i]);
+      }
+    }
+    else
+    {
+      printf("%s",b[k]);
+    }
+ return 0;
+}
