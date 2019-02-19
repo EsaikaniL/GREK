f562c9115e114534712d9e7b32bc83ba8601bad9
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-02-19 16:50:18 +0530

    Update spilt.py

--- spilt.py
+++ spilt.py
@@ -1,17 +1,5 @@
-n=input()
-a=[]
-b=[]
-for x in range(n):
-	k=input()
-	a.append(k)
-q=int(input())
-for x in range(q):
-	k=input()
-	b.append(k)
-for x in b:
-	k1=x.split(" ")[0].strip()
-	k2=x.split(" ")[1].strip()
-	ans=0
-	for y in range(int(k1),int(k2)+1):
-		ans=ans+int(a[y])
-	print(ans)
+n,q= map(int,input().split())
+N=[int(i) for i in input().split()]
+for _ in range(q):
+    u,v=map(int,input().split())
+    print(sum(N[u-1:v]))
