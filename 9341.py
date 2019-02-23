d492b3fcaa209c0c316a9e7b96e6c33e9a6ae3fd
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-02-23 16:50:11 +0530

    Update and rename 9341.c to 9341.py

---
+++ 9341.py
@@ -0,0 +1,25 @@
+//esaikani
+n=raw_input()
+c=1
+k=[]
+for x in range(0,len(n)-1):
+    if(n[x]==n[x+1]):
+        c=c+1
+    else:
+        if(c>1):
+            
+            k.append(c)
+            k.append("*")
+            k.append(n[x])
+            c=1
+        else:
+            k.append(n[x])
+if(c>1):
+    k.append(c)
+    k.append("*")
+    k.append(n[x])
+    
+else:
+    k.append(n[x+1])
+s="".join(map(str,k))
+print(s)
