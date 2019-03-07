ba1d9ca0b407abae3e47a5d2cb033107eed1332c
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-03-07 20:17:10 +0530

    Update arparn.c

--- arparn.c
+++ arparn.c
@@ -3,27 +3,12 @@
 
 int main(void) 
 {
-	char str[20];
-	int i,count1=0,count2=0;
+	char str[30];
 	scanf("%s",str);
-	for(i=0;str[i]!='\0';i++)
+	int i;
+	for(i=0;str[i]!='\0';i=i+3)
 	{
-		if(str[i]=='(')
-		{
-			count1=count1+1;
-		}
-		else if(str[i]==')')
-		{
-			count2=count2+1;
-		}
-	}
-	if(count1==count2)
-	{
-		printf("yes");
-	}
-	else
-	{
-		printf("no");
+		printf("%c",str[i]);
 	}
 	return 0;
 }
