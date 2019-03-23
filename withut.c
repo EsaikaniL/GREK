0087f0941b5a3eba6d3f3f8db28ffd9bca827660
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-03-23 18:39:47 +0530

    Update and rename witho9ut.c to withut.c

--- withut.c
+++ withut.c
@@ -5,11 +5,11 @@
 int main(void) 
 {
 	char str[50];
-	int i,count=0;
+	int i,count=1;
 	scanf("%[^\n]s",str);
 	for(i=0;str[i]!='\0';i++)
 	{
-		if(str[i]==' ')
+		if(str[i]=='.')
 		{
 			count=count+1;
 		}
