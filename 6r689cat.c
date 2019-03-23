d36134b850ddde6ff7de0b1a9b0f8efdb4c94fc6
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-03-23 20:24:48 +0530

    Update and rename 6r69cat.c to 6r689cat.c

---
+++ 6r689cat.c
@@ -0,0 +1,21 @@
+
+//esaikani
+#include <stdio.h>
+ 
+int main(void) 
+{
+	int n,m,pro;
+	scanf("%d",&n);
+	scanf("%d",&m);
+	pro=n*m;
+	if(pro%2==0)
+	{
+		printf("even");
+	}
+	else
+	{
+		printf("odd");
+	}
+	return 0;
+}
+ 
