fb50c8fb1d090e9ff01937dec0cea29cb6104630
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-03-24 14:25:57 +0530

    Update and rename doble.py to doble9.py

---
+++ doble9.py
@@ -0,0 +1,31 @@
+//esai
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	int n,sum=0,r,a,v=0,n1,s;
+	scanf("%d",&n);
+ n1=n;
+	while(n1!=0)
+	{
+		r=n1%10;
+		sum=sum+r;
+		n1=n1/10;
+	}
+	s=sum;
+	while(sum!=0)
+	{
+				a=sum%10;
+		v=(v*10)+(a);
+		sum=sum/10;
+
+	}
+	if(v==s)
+	{
+		printf("Yes");
+	}
+	else{
+	printf("No");
+	}
+	return 0;
+}
