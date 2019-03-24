05e8381c57edadfc041f5cbbd640e60ca9482df0
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-03-24 08:19:49 +0530

    Update and rename near.c to nea5r.c

---
+++ nea5r.c
@@ -0,0 +1,19 @@
+//esaikani
+#include <stdio.h>
+
+int main(void) 
+{
+	int n1,n2,diff;
+	scanf("%d",&n1);
+	scanf("%d",&n2);
+	diff=n1-n2;
+	if(diff%2==0)
+	{
+		printf("even");
+	}
+	else
+	{
+		printf("odd");
+	}
+	return 0;
+}
