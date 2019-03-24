bf61521ed6081b3a19edc3f6bed537a4a555a86e
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-03-24 08:16:55 +0530

    Update and rename 83.c to near3.c

--- near3.c
+++ near3.c
@@ -1,5 +1,18 @@
 //esaikani
-#include<stdio.h>
+#include <stdio.h>
+
+int main(void) 
+{
+	int num;
+	scanf("%d",&num);
+	num+=1;
+	while(num%10!=0)
+	{
+		num=num+1;
+	}
+	printf("%d",num);
+	return 0;
+}#include<stdio.h>
 
 int main(void)
 
