18ddc543de40bf887812a660e65858aca5f8f024
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-03-24 09:10:18 +0530

    Update 90n3er.c

--- 90n3er.c
+++ 90n3er.c
@@ -1,26 +1,21 @@
 //esai
 #include <stdio.h>
-#include<string.h>
+
 int main(void) 
 {
-	char str[50];
-	int i,flag=0;
-	scanf("%s",str);
-	for(i=0;str[i]!='\0';i++)
-	{
-		if(str[i]==str[i+1])
-		{
-			flag=1;
-			break;
-		}
-	}
-	if(flag==1)
-	{
-		printf("No");
-	}
-	else
-	{
-		printf("Yes");
-	}
-	return 0;
+int x,y,remainder,gcd;
+scanf("%d",&x);
+scanf("%d",&y);
+do
+{
+remainder=x%y;
+if(remainder==0)
+break;
+x=y;
+y=remainder;
+}
+while(remainder!=0);
+gcd=y;
+printf("%d",gcd);
+return 0;
 }
