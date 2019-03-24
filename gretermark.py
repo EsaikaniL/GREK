2c7c4ba590e8b5a650ba92f612f86a139edd465b
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-03-24 16:31:10 +0530

    Create gretermark.py

---
+++ gretermark.py
@@ -0,0 +1,14 @@
+//esaikani
+a=raw_input().split('#')
+b=raw_input().split('#')
+a1,b1=0,0
+for i in a:
+  if i.isdigit():
+    a1+=int(i)
+for i in b:
+  if i.isdigit():
+    b1+=int(i)
+if a1>b1:
+  print(a[0])
+else:
+  print(b[0])
