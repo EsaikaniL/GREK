c356549fe6efad61259de8296ea975cbfe93a258
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-05-26 10:44:31 +0530

    Update and rename porry.py to orry.py

---
+++ orry.py
@@ -0,0 +1,10 @@
+#esaikani
+a=input()
+if all(i=="a" or i=="b" for i in a):
+    print("yes")
+elif all(i=="a" for i in a):
+    print("yes")
+elif all(i=="b" for i in a):
+    print("yes")
+else:
+    print("no")
--- porry.py
+++
@@ -1,8 +0,0 @@
-#esaikani
-a=int(input())
-b=bin(a)
-count=0
-for i in range(0,len(b)):
-    if b[i]=="1":
-        count=count+1 
-print(count)
