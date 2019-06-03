0bd81c491597a638c25bfaed200613d48fa5199e
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-06-03 16:42:04 +0530

    Create modify.py

---
+++ modify.py
@@ -0,0 +1,11 @@
+#esaikani
+s=input()
+s=s.replace("a","*")
+s=s.replace("b","*")
+l=list(s)
+if l.count("*")==len(s):
+    print("yes")
+elif l.count("*")==len(s)-1:
+    print("yes")
+else:
+    print("no")
