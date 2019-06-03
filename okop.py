c652b19211aee36357cb36770055393d1d65a65f
EsaikaniL <46399336+EsaikaniL@users.noreply.github.com>
2019-06-03 17:36:53 +0530

    Update and rename oop.py to okop.py

--- okop.py
+++ okop.py
@@ -1,9 +1,9 @@
 #esaikani
 n,m=map(int,input().split())
 v=bin(n*m)
-c=0
+count=0
 for x in range(0,len(v)):
-    c=c+1
     if(v[x]=='1'):
-        print(c)
-        break
+    	count+=1
+print(count)
+ 
